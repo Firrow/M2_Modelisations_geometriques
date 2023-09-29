b6001bef796e82c8a52da827a72fdb2a393d45de
Firrow <firrowmb@gmail.com>
2023-09-29 15:44:15 +0200

    faces cylindre (marche pas)

--- HelloTriangle/Assets/Script/Cylinder.cs
+++ HelloTriangle/Assets/Script/Cylinder.cs
@@ -11,9 +11,10 @@ public class Cylinder : MonoBehaviour
 
     void Start()
     {
-        int numberPoints = numberMeridian * 2;
+        int numberPointsSides = numberMeridian * 2;
+        int numberPointsTotal = numberMeridian * 2 + 2;
 
-        Vector3[] vertices = new Vector3[numberMeridian * height];
+        Vector3[] vertices = new Vector3[numberMeridian * height + 2];
         Vector3 Pi = new Vector3();
         List<Vector3> triangles = new List<Vector3>();
 
@@ -44,22 +45,51 @@ public class Cylinder : MonoBehaviour
             }
         }
 
-   
+        //Point centre face du haut et du bas cylindre
+        Vector3 CentreHaut = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + height / 2, gameObject.transform.position.z);
+        Vector3 CentreBas = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - height / 2, gameObject.transform.position.z);
+        vertices[numberMeridian * height] = CentreHaut;
+        vertices[numberMeridian * height + 1] = CentreBas;
+        GameObject sphereHaut = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphereHaut.transform.position = CentreHaut;
+        sphereHaut.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+        GameObject sphereBas = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphereBas.transform.position = CentreBas;
+        sphereBas.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+
+
         //Création triangles
         for (int s = 0; s < numberMeridian; s++)
         {
-            //Création des triangles
             if (s == numberMeridian-1)
             {
-                triangles.Add(new Vector3(s, 0, numberPoints - 1)); //Triangle orienté haut
-                triangles.Add(new Vector3(0, numberPoints/2, numberPoints - 1)); //Triangle orienté bas
+                triangles.Add(new Vector3(s, 0, numberPointsSides - 1)); //Triangle orienté haut
+                triangles.Add(new Vector3(0, numberPointsSides/2, numberPointsSides - 1)); //Triangle orienté bas
             }
             else
             {
                 triangles.Add(new Vector3(s, s + 1, numberMeridian + s)); //Triangle orienté haut
-                triangles.Add(new Vector3(s + 1, numberPoints/2 + s + 1, numberPoints/2 + s)); //Triangle orienté bas
+                triangles.Add(new Vector3(s + 1, numberPointsSides/2 + s + 1, numberPointsSides/2 + s)); //Triangle orienté bas
             }
         }
+
+        for (int s = 0; s < numberPointsTotal; s++)
+        {
+            if (s < numberPointsTotal / 2)
+            {
+                triangles.Add(new Vector3(s, s + 1, numberPointsTotal-1));
+            }
+            else
+            {
+                triangles.Add(new Vector3(s, s + 1, numberPointsTotal));
+            }
+        }
+
+        foreach (var t in triangles)
+        {
+            Debug.Log("triangle : " + t);
+        }
+
 
         //TODO
         //Dessiner triangles en haut et en bas du cylindre (attention sens des normales)
@@ -74,6 +104,8 @@ public class Cylinder : MonoBehaviour
         msh.vertices = vertices;
 
         //Permet de passer d'une liste à un tableau de int
+        Debug.Log("nombre vertices : " + vertices.Length);
+        Debug.Log("nombre triangles : " + triangles.Count);
         int triangleTabSize = triangles.Count * 3;
         int[] triangleTab = new int[triangleTabSize];
         for (int i = 0; i < triangles.Count; i++)
