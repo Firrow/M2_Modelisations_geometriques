ecacb6886e4ef526c0755648bead1b6623f263f0
Firrow <73218766+Firrow@users.noreply.github.com>
2023-10-02 10:41:05 +0200

    cylinder work (only for height = 2)

--- HelloTriangle/Assets/Script/Cylinder.cs
+++ HelloTriangle/Assets/Script/Cylinder.cs
@@ -11,8 +11,8 @@ public class Cylinder : MonoBehaviour
 
     void Start()
     {
-        int numberPointsSides = numberMeridian * 2;
-        int numberPointsTotal = numberMeridian * 2 + 2;
+        int numberPointsSides = numberMeridian * height;
+        int numberPointsTotal = numberMeridian * height + 2;
 
         Vector3[] vertices = new Vector3[numberMeridian * height + 2];
         Vector3 Pi = new Vector3();
@@ -27,7 +27,6 @@ public class Cylinder : MonoBehaviour
                 //Création des points de la grille
                 if (j == 1)
                 {
-                    Debug.Log("Bas !");
                     Pi = new Vector3(radius * Mathf.Cos((2 * Mathf.PI * i) / numberMeridian), -height / 2, radius * Mathf.Sin((2 * Mathf.PI * i) / numberMeridian));
                 }
                 else
@@ -50,7 +49,7 @@ public class Cylinder : MonoBehaviour
         Vector3 CentreBas = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - height / 2, gameObject.transform.position.z);
         vertices[numberMeridian * height] = CentreHaut;
         vertices[numberMeridian * height + 1] = CentreBas;
-        GameObject sphereHaut = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        GameObject sphereHaut = GameObject.CreatePrimitive(PrimitiveType.Sphere); //partie temporaire
         sphereHaut.transform.position = CentreHaut;
         sphereHaut.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
         GameObject sphereBas = GameObject.CreatePrimitive(PrimitiveType.Sphere);
@@ -73,39 +72,38 @@ public class Cylinder : MonoBehaviour
             }
         }
 
+        //création sommet cylindre
         for (int s = 0; s < numberPointsTotal; s++)
         {
-            if (s < numberPointsTotal / 2)
+            if (s <= numberPointsTotal / 2 - 2)
             {
-                triangles.Add(new Vector3(s, s + 1, numberPointsTotal-1));
+                if (s == numberPointsTotal / 2 - 2)
+                {
+                    triangles.Add(new Vector3(numberPointsTotal - 1, 0, s));
+                }
+                else
+                {
+                    triangles.Add(new Vector3(numberPointsTotal - 1, s + 1, s));
+                }
             }
-            else
+            else if (s >= numberPointsTotal / 2 - 1 && s < numberPointsTotal - 2)
             {
-                triangles.Add(new Vector3(s, s + 1, numberPointsTotal));
+                if (s == numberPointsTotal - 3)
+                {
+                    triangles.Add(new Vector3(s, numberPointsTotal / 2 - 1, numberPointsTotal - 1));
+                }
+                else
+                {
+                    triangles.Add(new Vector3(s, s + 1, numberPointsTotal - 1));
+                }
             }
         }
-
-        foreach (var t in triangles)
-        {
-            Debug.Log("triangle : " + t);
-        }
-
-
-        //TODO
-        //Dessiner triangles en haut et en bas du cylindre (attention sens des normales)
-
 
         Mesh msh = new Mesh();                          
 
-
-        gameObject.AddComponent<MeshFilter>();          
-        gameObject.AddComponent<MeshRenderer>();
-
         msh.vertices = vertices;
 
-        //Permet de passer d'une liste à un tableau de int
-        Debug.Log("nombre vertices : " + vertices.Length);
-        Debug.Log("nombre triangles : " + triangles.Count);
+
         int triangleTabSize = triangles.Count * 3;
         int[] triangleTab = new int[triangleTabSize];
         for (int i = 0; i < triangles.Count; i++)
@@ -119,18 +117,6 @@ public class Cylinder : MonoBehaviour
 
         gameObject.GetComponent<MeshFilter>().mesh = msh;           // Remplissage du Mesh et ajout du matériel
         gameObject.GetComponent<MeshRenderer>().material = material;
-    }
-
-    void Update()
-    {
-        
-    }
-
-
-
-    public void CreateWallTriangles(int numberMeridian, int height, int radius, Material material)
-    {
-
     }
 
 }
