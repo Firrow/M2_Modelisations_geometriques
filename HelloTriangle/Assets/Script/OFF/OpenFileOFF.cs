f4a55ca83ad32c7739e7e41db1fb4feca1b9dc6f
Firrow <firrowmb@gmail.com>
2023-10-20 14:10:24 +0200

    Setting up part OFF file

---
+++ HelloTriangle/Assets/Script/OFF/OpenFileOFF.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenFileOFF : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
