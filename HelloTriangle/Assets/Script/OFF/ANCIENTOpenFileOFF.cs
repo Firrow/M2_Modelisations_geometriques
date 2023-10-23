9813d3eca86b095bb7c6976c86ab1c0e1def9872
Firrow <73218766+Firrow@users.noreply.github.com>
2023-10-23 08:55:51 +0200

    Question 3 et 4

---
+++ HelloTriangle/Assets/Script/OFF/ANCIENTOpenFileOFF.cs
@@ -0,0 +1,84 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+using System;
+using System.Globalization;
+
+public class ANCIENTOpenFileOFF : MonoBehaviour
+{
+    public string file;
+    public Material material;
+
+    private string filePath;
+
+    void Start()
+    {
+        filePath = "Assets\\Script\\OFF\\" + file;
+        OffReader offReader = new OffReader(filePath);
+
+        Mesh msh = new Mesh();
+
+        msh.vertices = offReader.getVertices();
+        msh.triangles = offReader.getTriangles();
+
+        gameObject.GetComponent<MeshFilter>().mesh = msh;
+        gameObject.GetComponent<MeshFilter>().mesh.RecalculateNormals();
+        gameObject.GetComponent<MeshRenderer>().material = material;
+    }
+}
+
+class OffReader
+{
+    private string filePath;
+    private int numberOfVertices;
+    private int numberOfTriangles;
+    private string[] lineTab;
+
+    public OffReader(string filePath)
+    {
+        this.filePath = filePath;
+        init();
+    }
+
+    private void init()
+    {
+        var lines = File.ReadAllLines(filePath).Select(s => s.Trim());
+        lineTab = lines.ToArray();
+
+        if (lineTab[0] != "OFF")
+            throw new Exception("You are not reading a OFF file");
+
+        this.numberOfVertices = int.Parse(lineTab[1].Split(' ')[0]);
+        this.numberOfTriangles = int.Parse(lineTab[1].Split(' ')[1]);
+    }
+
+    public Vector3[] getVertices()
+    {
+        Vector3[] vertices = new Vector3[numberOfVertices];
+        for (int i = 0; i < numberOfVertices; i++)
+        {
+            float[] vertex = lineTab[i + 2].Split(' ').Select(s => float.Parse(s, CultureInfo.InvariantCulture)).ToArray();
+            vertices[i] = new Vector3(vertex[0], vertex[1], vertex[2]);
+
+            //GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            //sphere.transform.position = vertices[i];
+            //sphere.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+        }
+        return vertices;
+    }
+
+    public int[] getTriangles()
+    {
+        int[] triangles = new int[numberOfTriangles*3];
+        for (int i = 0; i < numberOfTriangles; i++)
+        {
+            int[] vertex = lineTab[i + 2 + numberOfVertices].Split(' ').Select(s => int.Parse(s)).ToArray();
+            triangles[i*3] = vertex[1];
+            triangles[i*3+1] = vertex[2];
+            triangles[i*3+2] = vertex[3];
+        }
+        return triangles;
+    }
+}*/
--- HelloTriangle/Assets/Script/OFF/OpenFileOFF.cs
+++ HelloTriangle/Assets/Script/OFF/OpenFileOFF.cs
@@ -9,24 +9,29 @@ using System.Globalization;
 public class OpenFileOFF : MonoBehaviour
 {
     public string file;
-    public Material material;
 
     private string filePath;
 
     void Start()
     {
-        filePath = "Assets\\Script\\OFF\\" + file;
+        //string separator = Path.DirectorySeparatorChar.ToString();
+        filePath = Application.dataPath + "/Files/" + file;
+        Debug.Log(filePath);
         OffReader offReader = new OffReader(filePath);
 
-        Mesh msh = new Mesh();
+        MyMesh myMesh = new MyMesh(offReader.getVertices(), offReader.getTriangles());
+        myMesh.center();
+        myMesh.normalize();
 
-        msh.vertices = offReader.getVertices();
-        msh.triangles = offReader.getTriangles();
+        Mesh msh = new Mesh();
+        msh.vertices = myMesh.getVertices();
+        msh.triangles = myMesh.getTriangles();
 
         gameObject.GetComponent<MeshFilter>().mesh = msh;
-        gameObject.GetComponent<MeshFilter>().mesh.RecalculateNormals();
-        gameObject.GetComponent<MeshRenderer>().material = material;
     }
+
+
+
 }
 
 class OffReader
@@ -61,10 +66,6 @@ class OffReader
         {
             float[] vertex = lineTab[i + 2].Split(' ').Select(s => float.Parse(s, CultureInfo.InvariantCulture)).ToArray();
             vertices[i] = new Vector3(vertex[0], vertex[1], vertex[2]);
-
-            /*GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            sphere.transform.position = vertices[i];
-            sphere.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);*/
         }
         return vertices;
     }
@@ -80,5 +81,60 @@ class OffReader
             triangles[i*3+2] = vertex[3];
         }
         return triangles;
+    }
+}
+
+class MyMesh {
+
+    private Vector3[] vertices;
+    private int[] triangles;
+
+    public MyMesh(Vector3[] vertices, int[] triangles) {
+        this.vertices = vertices;
+        this.triangles = triangles;
+    }
+
+    public Vector3[] getVertices() {
+        return vertices;
+    }
+
+    public int[] getTriangles() {
+        return triangles;
+    }
+
+    public void center() {
+        Vector3 meshCenter = getGravityCenter();
+        Vector3 meshCenterToSceneCenter = meshCenter - Vector3.zero;
+        for (int i = 0; i < vertices.Length; i++) {
+            vertices[i] -= meshCenterToSceneCenter;
+        }
+    }
+
+    public void normalize() {
+        float biggerDistanceToCenter = getBiggerDistanceToCenter();
+        for (int i = 0; i < vertices.Length; i++) {
+            vertices[i] /= biggerDistanceToCenter;
+        }
+    }
+
+    private float getBiggerDistanceToCenter() {
+        float biggerDistanceToCenter = 0;
+        Vector3 gravityCenter = getGravityCenter();
+        for (int i = 0; i < vertices.Length; i++) {
+            float distanceToCenter = Vector3.Distance(vertices[i], gravityCenter);
+            if (distanceToCenter > biggerDistanceToCenter) {
+                biggerDistanceToCenter = distanceToCenter;
+            }
+        }
+        return biggerDistanceToCenter;
+    }
+
+    private Vector3 getGravityCenter() {
+        Vector3 gravityCenter = Vector3.zero;
+        for (int i = 0; i < vertices.Length; i++) {
+            gravityCenter += vertices[i];
+        }
+        gravityCenter /= vertices.Length;
+        return gravityCenter;
     }
 }
