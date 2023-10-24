096a7319b63bb423c807d486f4ec51de461fd5b1
Firrow <73218766+Firrow@users.noreply.github.com>
2023-10-24 11:40:40 +0200

    Question 5

--- HelloTriangle/Assets/Script/Volumes/VolumesCube.cs
+++ HelloTriangle/Assets/Script/Volumes/VolumesCube.cs
@@ -1,22 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class VolumesCube : MonoBehaviour
 {
     public string operation;
-    public float radiusSphere1;
-    public float radiusSphere2;
-    public Vector3 centerSphere1;
-    public Vector3 centerSphere2;
+    public List<SphereVolume> sphereList = new List<SphereVolume>();
 
-    private float boundingBoxSize;
-    private float numberCubeOnEdge;
+    public float boundingBoxSize;
+    public float numberCubeOnEdge;
 
     void Start()
     {
         //Dessiner cubes
+        SphereVolume s1 = new SphereVolume(3, new Vector3(6, 5, 5));
+        SphereVolume s2 = new SphereVolume(3, new Vector3(2, 2, 2));
+        SphereVolume s3 = new SphereVolume(3, new Vector3(0, 0, 3));
+        sphereList.Add(s1);
+        sphereList.Add(s2);
+        sphereList.Add(s3);
+
         CreateBoundingBox();
     }
 
@@ -33,71 +38,92 @@ public class VolumesCube : MonoBehaviour
             {
                 for (int k = 0; k < numberCubeOnEdge; k++)
                 {
-                    Vector3 posCube = new Vector3(((boundingBoxSize) / numberCubeOnEdge) * i - boundingBoxSize / 2, ((boundingBoxSize) / numberCubeOnEdge) * j - boundingBoxSize / 2, ((boundingBoxSize) / numberCubeOnEdge) * k - boundingBoxSize / 2);
+                    Vector3 posPetitCube = new Vector3(((boundingBoxSize) / numberCubeOnEdge) * i - boundingBoxSize / 2, ((boundingBoxSize) / numberCubeOnEdge) * j - boundingBoxSize / 2, ((boundingBoxSize) / numberCubeOnEdge) * k - boundingBoxSize / 2);
 
-                    ChoiceUser(operation, posCube, centerSphere1, centerSphere2, radiusSphere1, radiusSphere2);
+                    ChoiceUser(operation, posPetitCube);
                 }
             }
         }
     }
 
-    private void Intersect(Vector3 posCube, Vector3 CenterSphere1, Vector3 CenterSphere2, float radiusSphere1, float radiusSphere2)
+    private void Intersect(Vector3 posCube) //Vector3 posCube, Vector3 CenterSphere1, Vector3 CenterSphere2, float radiusSphere1, float radiusSphere2
     {
-        if (IsInside(posCube, centerSphere1, radiusSphere1) && IsInside(posCube, centerSphere2, radiusSphere2))
+        if (IsInsideIntersection(posCube))
         {
             DrawCube(posCube);
         }
     }
 
-    private void Union(Vector3 posCube, Vector3 CenterSphere1, Vector3 CenterSphere2, float radiusSphere1, float radiusSphere2)
+    private void Union(Vector3 posPetitCube)
     {
-        if (IsInside(posCube, centerSphere1, radiusSphere1) || IsInside(posCube, centerSphere2, radiusSphere2))
+        if (IsInsideUnion(posPetitCube))
         {
-            DrawCube(posCube);
+            DrawCube(posPetitCube);
         }
     }
 
-    private void DrawCube(Vector3 posCube)
+    private void DrawCube(Vector3 posPetitCube)
     {
         GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube.transform.position = posCube;
+        cube.transform.position = posPetitCube;
         cube.transform.localScale = new Vector3(boundingBoxSize / numberCubeOnEdge, boundingBoxSize / numberCubeOnEdge, boundingBoxSize / numberCubeOnEdge);
     }
 
 
 
 
-    private bool IsInside(Vector3 posCube, Vector3 CenterSphere, float radiusSphere)
+    private bool IsInsideIntersection(Vector3 posCube) //Vector3 posCube, Vector3 CenterSphere, float radiusSphere
     {
-        return Math.Pow(posCube.x - CenterSphere.x, 2) + Math.Pow(posCube.y - CenterSphere.y, 2) + Math.Pow(posCube.z - CenterSphere.z, 2) - Math.Pow(radiusSphere, 2) < 0;
+        bool[] inside = new bool[this.sphereList.Count];
+        for (int i = 0; i < this.sphereList.Count; i++)
+        {
+            inside[i] = Math.Pow(posCube.x - sphereList[i].centerSphere.x, 2) + Math.Pow(posCube.y - sphereList[i].centerSphere.y, 2) + Math.Pow(posCube.z - sphereList[i].centerSphere.z, 2) - Math.Pow(sphereList[i].radiusSphere, 2) < 0;
+        }
+
+        return inside.All(val => val == true);
     }
 
-    private void ChoiceUser(string operation, Vector3 posCube, Vector3 CenterSphere1, Vector3 CenterSphere2, float radiusSphere1, float radiusSphere2)
+    private bool IsInsideUnion(Vector3 posCube) //Vector3 posCube, Vector3 CenterSphere, float radiusSphere
+    {
+        bool[] inside = new bool[this.sphereList.Count];
+        for (int i = 0; i < this.sphereList.Count; i++)
+        {
+            inside[i] = Math.Pow(posCube.x - sphereList[i].centerSphere.x, 2) + Math.Pow(posCube.y - sphereList[i].centerSphere.y, 2) + Math.Pow(posCube.z - sphereList[i].centerSphere.z, 2) - Math.Pow(sphereList[i].radiusSphere, 2) < 0;
+        }
+
+        return inside.Any(val => val == true);
+    }
+
+    private void ChoiceUser(string operation, Vector3 posPetitCube) //string operation, Vector3 posCube, Vector3 CenterSphere1, Vector3 CenterSphere2, float radiusSphere1, float radiusSphere2
     {
         if (operation == "intersection")
-            Intersect(posCube, CenterSphere1, CenterSphere2, radiusSphere1, radiusSphere2);
+            Intersect(posPetitCube);
         else if (operation == "union")
-            Union(posCube, CenterSphere1, CenterSphere2, radiusSphere1, radiusSphere2);
+            Union(posPetitCube);
         else
-            throw new Exception("Opération inconnue du système.");
+            DrawCube(posPetitCube);
+            //throw new Exception("Opération inconnue du système.");
     }
 
-    /*private void CalculSizeBoundingBox(Vector3 CenterSphere1, Vector3 CenterSphere2, float radiusSphere1, float radiusSphere2)
+    private void CalculSizeBoundingBox()
     {
         //calculer la taille de la taille englobante en fonction sphere
-        //calculer la taille des petits cubes dans la boite
-    }*/
-} 
 
-public class Sphere
+        //calculer la taille des petits cubes dans la boite
+    }
+}
+
+public class SphereVolume
 {
     public float radiusSphere;
     public Vector3 centerSphere;
 
-    public Sphere(float radius, Vector3 position)
+    public SphereVolume(float radius, Vector3 position)
     {
         this.radiusSphere = radius;
         this.centerSphere = position;
     }
 }
+
+
 
